a9f41b848d150e50ebb0d12dadd0a6afe9da9fd5
alupar <memoria@bk.ru>
2020-12-07 18:08:51 +0300

    Add day1

--- src/main/java/day1/Task1.java
+++ src/main/java/day1/Task1.java
@@ -2,6 +2,13 @@ package day1;
 
 public class Task1 {
     public static void main(String[] args) {
+        int value = 0;
+        String word = "JAVA";
+        while (value < 10) {
+            System.out.print(word + " ");
+            value++;
+
+        }
 
     }
 }

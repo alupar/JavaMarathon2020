0141c5dd1c2487559d8909486ee3ac7895a8c3b9
alupar <memoria@bk.ru>
2020-12-07 21:27:27 +0300

    Add day2

--- src/main/java/day2/Task1.java
+++ src/main/java/day2/Task1.java
@@ -1,7 +1,20 @@
 package day2;
 
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
-
+        //System.out.println("Введите количество этажей");
+        Scanner scanner = new Scanner(System.in);
+        int stage = scanner.nextInt();
+        if (stage > 0 && stage < 5) {
+            System.out.println("Малоэтажный дом");
+        } else if (stage >= 5 && stage < 9) {
+            System.out.println("Среднеэтажный дом");
+        } else if (stage >= 9) {
+            System.out.println("Многоэтажный дом");
+        } else {
+            System.out.println("Ошибка ввода");
+        }
     }
 }
--- src/main/java/day2/Task3.java
+++ src/main/java/day2/Task3.java
@@ -1,7 +1,22 @@
 package day2;
 
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = a + 1;
+        if (a >= b) {
+            System.out.println("Некорректный ввод");
+        } else {
+            while (c < b) {
+                if (c % 5 == 0 && c % 10 != 0) {
+                    System.out.print(c + " ");
+                }
+                c++;
+            }
+        }
     }
 }

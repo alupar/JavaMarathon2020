a146017a1928b470c3b3795a7acbd7280a3f66ab
alupar <memoria@bk.ru>
2020-12-08 10:32:33 +0300

    Add day3

--- src/main/java/day3/Task3.java
+++ src/main/java/day3/Task3.java
@@ -1,7 +1,21 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
+        int i = 0;
+        Scanner scanner = new Scanner(System.in);
+        while (i < 5) {
+            double a = scanner.nextDouble();
+            double b = scanner.nextDouble();
+            i++;
+            if (b == 0) {
+                System.out.println("Деление на 0");
+                continue;
+            }
+            System.out.println(a / b);
 
+        }
     }
 }
